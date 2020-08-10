e5a129fb8efa8248da18265aae2fcb177af9b6d0
Abdul Lathif <lathif.is10@gmail.com>
2020-08-10 19:04:36 +0700

    first init

---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <pre>{{stocks}}</pre>
+        <app-stock v-for="(stock, index) in stocks" :key="index" :stock="stock"></app-stock>
+    </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+import Stock from './Stock.vue'
+
+export default {
+    mounted() {
+        console.log(mapGetters)
+    },
+    computed: {
+        ...mapGetters({
+            stocks: 'stockPortfolio'
+        })
+    },
+    components: {
+        appStock: Stock
+    }
+}
+</script>
